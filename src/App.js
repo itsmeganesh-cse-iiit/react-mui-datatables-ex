a90cc3319666af60cc77cf6993be71272f33cea9
Ganesh Koilada <ganesh@spheremi.com>
2020-03-26 23:09:58 +0530

    init"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MUIDataTable from "mui-datatables";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: ""
+    };
+  }
+
+  onRowClick = props => {
+    this.setState({
+      searchText: props[0]
+    });
+  };
+
+  customToolbar = props => {
+    console.log("g", props);
+  };
+  render() {
+    const columns = ["Name", "Title", "Location", "Age", "Salary"];
+
+    const data = [
+      ["Gabby", "Business Analyst", "Minneapolis", 30, "$100,000"],
+      ["Aiden", "Business Consultant", "Dallas", 55, "$200,000"],
+      ["Jaden", "Attorney", "Santa Ana", 27, "$500,000"],
+      ["Franky", "Business Analyst", "St. Petersburg", 22, "$50,000"],
+      ["Gabby George", "Business Consultant", "Toledo", 28, "$75,000"]
+    ];
+
+   
+
+    return (
+      <MUIDataTable
+        title={["ACME Employee list"]}
+        data={data}
+        columns={columns}
+        selectableRowsOnClick={false}
+        options={{
+          onRowClick: this.onRowClick,
+          searchText: this.state.searchText,
+          customToolbar: this.customToolbar
+        }}
+      />
+    );
+  }
 }
 
 export default App;
